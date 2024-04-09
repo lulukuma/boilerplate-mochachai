13c6a29f3fdad14876da572b2f8a4a25187f8a2e
lulu kuma <brookehdiaz@gmail.com>
2024-04-09 15:10:37 +0000

    fox

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -63,7 +63,7 @@ suite('Unit Tests', function () {
     test('#isAbove, #isAtMost', function () {
       assert.isAtMost('hello'.length, 5);
       assert.isAbove(1, 0);
-      assert.isAtMost(Math.PI, 3);
+      assert.isAbove(Math.PI, 3);
       assert.isAtMost(1 - Math.random(), 1);
     });
     // #9
