ecf0b4083cd8eae8d56e2dfdc0085e17ac7f1615
lulu kuma <brookehdiaz@gmail.com>
2024-05-13 20:03:12 +0000

    more adjustments

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -41,13 +41,10 @@ suite('Functional Tests', function () {
         .put('/travellers')
 
         .end(function (err, res) {
-          //assert.fail();
           assert.equal(res.status, 200, 'response status should be 200');
           assert.equal(res.type, 'application/json', 'Response should be json');
-          assert.equal(res.body.name, 'Cristoforo', 'res.body.name should be Cristoforo');
-          assert.equal(res.body.surname, 'Colombo', 'res.body.surname should be Colombo');
-
-
+          assert.equal(res.body.name, 'Cristoforo', 'res.body.name should be "Cristoforo"');
+          assert.equal(res.body.surname, 'Colombo', 'res.body.surname should be "Colombo"');
           done();
         });
     });
